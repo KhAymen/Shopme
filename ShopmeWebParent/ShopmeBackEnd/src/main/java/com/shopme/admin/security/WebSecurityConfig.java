e9177f41d79a7de3bbb66aa97d6b3e2377172009
Khelifi Aymen <khelifi.aymen@hotmail.com>
2025-07-06 12:38:01 +0100

    Enable Remember Me for Login

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -15,43 +15,49 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    UserDetailsService userDetailsService() {
+	@Bean
+	UserDetailsService userDetailsService() {
 		return new ShopmeUserDetailsService();
 	}
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
+	@Bean
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService());
 		authProvider.setPasswordEncoder(passwordEncoder());
-		
+
 		return authProvider;
 	}
-	
+
 	@Bean
-	SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {		
+	SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
+		http.authenticationProvider(authenticationProvider());
+
 		http.authorizeHttpRequests(auth -> auth
 				.anyRequest().authenticated()
 			)
-			.formLogin(form -> form			
+			.formLogin(form -> form
 				.loginPage("/login")
 				.usernameParameter("email")
 				.permitAll())
-			
-			.logout(logout -> logout.permitAll());
-			
+
+			.logout(logout -> logout.permitAll())
+
+			.rememberMe(rem -> rem
+					.key("AbcDefgHijKlmnOpqrs_1234567890")
+					.tokenValiditySeconds(7 * 24 * 60 * 60));
+
 			return http.build();
 	}
-	
+
 	@Bean
 	WebSecurityCustomizer configureWebSecurity() throws Exception {
 		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
 	}
- 
+
 }

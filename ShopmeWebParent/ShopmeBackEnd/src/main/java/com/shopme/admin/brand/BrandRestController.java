eae724c7a6f8ad29de0b590964cdf436ccae5ec6
Khelifi Aymen <khelifi.aymen@hotmail.com>
2025-11-29 11:54:03 +0100

    Fix -- No check Unique Name for New Brand

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRestController.java
@@ -1,8 +1,8 @@
 package com.shopme.admin.brand;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +12,7 @@ public class BrandRestController {
 	private BrandService service;
 
 	@PostMapping("/brands/check_unique")
-	public String checkUnique(@RequestParam("id") Integer id, @RequestParam("name") String name) {
+	public String checkUnique(@Param("id") Integer id, @Param("name") String name) {
 		return service.checkUnique(id, name);
 	}
 }
